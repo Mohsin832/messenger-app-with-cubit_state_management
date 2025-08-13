dd780b8700b57080211aa1d9266eaeb38f8ae377
Mohsin832 <mm8116900@gmail.com>
2025-08-13 09:36:16 -0700

    otp screen working

--- lib/repository/screens/OTP/otpscreen.dart
+++ lib/repository/screens/OTP/otpscreen.dart
@@ -1,4 +1,5 @@
 import 'package:chatapp/domain/constants/appcolors.dart';
+import 'package:chatapp/repository/screens/widgets/uihelper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -17,6 +18,34 @@ class Otpscreen extends StatelessWidget {
             Navigator.pop(context);
           },
           icon: Icon(CupertinoIcons.back),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 40),
+            Uihelper.customText(
+              text: "Enter Code",
+              fontsize: 24,
+              context: context,
+              fontfamily: "bold",
+              fontweight: FontWeight.bold,
+            ),
+            SizedBox(height: 40),
+
+            Uihelper.customText(
+              text: "We have sent you an SMS with the code",
+              fontsize: 14,
+              context: context,
+              fontweight: FontWeight.w400,
+            ),
+            Uihelper.customText(
+              text: "to +62 1309 - 1710 - 1920",
+              fontsize: 14,
+              context: context,
+              fontweight: FontWeight.w400,
+            ),
+          ],
         ),
       ),
     );
